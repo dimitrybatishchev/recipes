f093997e474ec0f47908990d8f169219450bc0d6
dimitrybatishchev <dimitrybatishchev@gmail.com>
2014-03-09 11:16:32 +0400

    prod env edited

--- web/app.php
+++ web/app.php
@@ -13,6 +13,7 @@ $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
 */
 
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
